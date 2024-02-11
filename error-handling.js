4d7543fae6a51987ef9a45801b2354a751acd0b3
dirtroaddev <rongoodwindev@gmail.com>
2024-02-10 23:01:01 -0500

    Error handling

--- error-handling.js
+++ error-handling.js
@@ -1,10 +1,15 @@
 // 1.
 function sum(array) {
+
+  try {
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
   return sum;
+} catch (e) {
+  console.log(e.message);
+}
 }
 
 let res = sum(null);
@@ -12,15 +17,32 @@ console.log(res);
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+try {
+  sayName("Alex");
+  sayName(1);
+} catch(e) {
+  console.log(e.message);
+}
+
 // Your code here
 
+function sayName(name) {
+  if(name !== 'string') {
+    throw new Error("Must be a string!");
+  } else {
+    console.log(name);
+  }
+}
 // 3.
 function greet(greeting) {
-  if (!greeting) {
-    throw new Error("There was no greeting given.");
+  try {
+    if (!greeting) {
+      throw new Error("There was no greeting given.");
+    }
+  } catch(e) {
+
   }
+
 
   console.log(greeting);
 }
